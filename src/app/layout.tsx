553ec9f2526ec1336fb9a01babcd0ef96346d8a1
Mohamed Shakir <mshak084@uottawa.ca>
2025-04-05 17:43:55 -0400

    updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,17 +28,13 @@ export default function RootLayout({
       <head>
         <meta name="apple-mobile-web-app-title" content="findmyprof" />
 
-        <meta property="og:url" content="https://www.findmyprofessor.ca/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Find My Professor" />
-        <meta
-          property="og:description"
-          content="Find professors and connect with their expertise."
-        />
         <meta
           property="og:image"
-          content="https://opengraph.b-cdn.net/production/images/a16b4b84-c7c4-4da0-b02d-065cab697c34.png?token=shzmIjmQXTuO2vzDmikDyN5JKv7S1mDslHKMDW2vClc&height=462&width=1200&expires=33279888748"
+          content="https://www.findmyprofessor.ca/thumbnail.png"
         />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="2254" />
+        <meta property="og:image:height" content="868" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="findmyprofessor.ca" />
